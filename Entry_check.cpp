5ab97524c06c67135834e6f20bd36cc9881193d4
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-01 07:34:35 +0300

    Add files via upload
    first input x value and then compare that's it

---
+++ Entry_check.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x;
+  cin >> x;
+  if (x>=10)
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
